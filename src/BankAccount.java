bcad147d246cf199d026f9cce1ac4ba1c8d3c8b5
Gia Bao Vu <25vll@queensu.ca>
2026-09-10 12:16:32 -0400

    Implement BankAccount class with deposit and withdraw methods

---
+++ src/BankAccount.java
@@ -0,0 +1,39 @@
+public class BankAccount {
+    private String accountHolder;
+    private double balance;
+
+    public BankAccount(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+        System.out.println("Deposited" + amount + " to" + accountHolder + "'s account . New balance is " + balance);
+
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdraw" + amount + " from" + accountHolder + "'s account. New balance is " + balance);
+            return true;
+
+        } else {
+            System.out.println("Insufficient funds for withdraw of" + amount + " from " + accountHolder
+                    + "'s accoount. Current balance is " + balance);
+
+            return false;
+
+        }
+    }
+
+}
